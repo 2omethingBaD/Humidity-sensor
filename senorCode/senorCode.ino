bd1bc1b211634aff2beb62280a47a5fda4ef8832
2omethingBaD <113650499+2omethingBaD@users.noreply.github.com>
2023-11-15 21:28:09 -0500

    fixed error code dispaly

--- senorCode/senorCode.ino
+++ senorCode/senorCode.ino
@@ -16,7 +16,6 @@ DHT dht(sensor_pin, DHTTYPE);
 Adafruit_SSD1306 display(screen_w, screen_h, &Wire, reset_pin);
 
 String message;
-int text_size;
 
 
 void setup() {
@@ -52,16 +51,19 @@ void loop() {
 
   // check if any reads failed
   if (isnan(humi) || isnan(temp)) {
-    message = "Help!";
-  }
-
-  // controls motor
-  if (humi <= 62 && !isnan(humi) || !isnan(temp)) {
-    digitalWrite(motor_pin, HIGH);
-    message = "  " + String(temp) + " F   " + String(humi) + " %" + "  ~~~~~~~~~" +" motor on";
+    message = "       ~Help!~";
+    display.setTextSize(3);
   } else {
-    digitalWrite(motor_pin, LOW);
-    message = "  " + String(temp) + " F   " + String(humi) + " %" + "  ~~~~~~~~~" +" motor off";
+    // controls motor
+    if (humi <= 62) {
+      digitalWrite(motor_pin, HIGH);
+      message = "  " + String(temp) + " F   " + String(humi) + " %" + "  ~~~~~~~~~" +" motor on";
+      display.setTextSize(2);
+    } else {
+      digitalWrite(motor_pin, LOW);
+      message = "  " + String(temp) + " F   " + String(humi) + " %" + "  ~~~~~~~~~" +" motor off";
+      display.setTextSize(2);
+    }
   }
 
   // olde dispaly configs
@@ -70,16 +72,8 @@ void loop() {
     display.clearDisplay(); //clear display
   }
 
-  if (message.length() > 0 && message.length() <= 5) { // messages between 1 and 5 characters activates text-size 3
-    display.setTextSize(3);
-    Serial.println("display has been updated with: " + message); // print the message in Serial monitor for feedback
-  }
-
-  if (message.length() >= 21 || message.length() > 6 &&  message.length() <= 20) { // messages longer than 21 or between 6 and 20 characters activates text-size 2
-    display.setTextSize(2);
-    Serial.println("display has been updated with: " + message); // print the message in Serial monitor for feedback
-  }
-
+  Serial.println("display has been updated with: " + message); // print the message in Serial monitor for feedback
+  
   display.setCursor(0, 0);//start printing at top left corner 
   display.println(message); //prints the content of the message string on the display
   display.display(); //transfers the buffer to the display
